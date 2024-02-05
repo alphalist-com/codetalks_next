84a5bcad54f03a2c69e442cb6e1b668ef9463790
Martin Westphal <martin@westphal.pw>
2024-02-05 19:39:06 +0100

    handle non additional person tile

--- components/PersonTile.vue
+++ components/PersonTile.vue
@@ -1,40 +1,48 @@
 <template>
-  <div class="relative flex flex-col items-center w-36 h-56 sm:w-40 sm:h-64 cursor-pointer">
+  <div>
     <div
-      class="relative w-full h-full cota-material rounded-lg overflow-hidden"
+      :class="[
+        'relative flex flex-col items-center w-36 h-56 sm:w-40 sm:h-64',
+        person.detailedInfos ? 'cursor-pointer' : '',
+      ]"
+      @click=""
     >
-      <img
-        class="w-full h-full object-cover"
-        :src="props.person.image"
-        :alt="person.name"
-      />
       <div
-        class="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-2"
+        class="relative w-full h-full cota-material rounded-lg overflow-hidden"
       >
-        <div class="grid grid-cols-10">
-          <p
-            class="col-span-10 text-md text-clip line-clamp-1 font-semibold text-white"
-          >
-            {{ person.name }}
-          </p>
-          <p
-            v-if="person.position"
-            class="col-span-10 text-sm text-ellipsis line-clamp-1 text-gray-300"
-          >
-            {{ person.position }}
-          </p>
-          <p
-            v-if="person.company"
-            class="col-span-9 text-sm text-clip h-4 line-clamp-1 text-gray-300"
-          >
-            {{ person.company }}
-          </p>
-          <button
-            v-if="person.detailedInfos"
-            class="col-span-1 items-center justify-center w-4 h-4 cota-material rounded-full"
-          >
-            <p class="text-cota-primary text-xs">i</p>
-          </button>
+        <img
+          class="w-full h-full object-cover"
+          :src="props.person.image"
+          :alt="person.name"
+        />
+        <div
+          class="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-2"
+        >
+          <div class="grid grid-cols-10">
+            <p
+              class="col-span-10 text-md text-clip line-clamp-1 font-semibold text-white"
+            >
+              {{ person.name }}
+            </p>
+            <p
+              v-if="person.position"
+              class="col-span-10 text-sm text-ellipsis line-clamp-1 text-gray-300"
+            >
+              {{ person.position }}
+            </p>
+            <p
+              v-if="person.company"
+              class="col-span-9 text-sm text-clip h-4 line-clamp-1 text-gray-300"
+            >
+              {{ person.company }}
+            </p>
+            <button
+              v-if="person.detailedInfos"
+              class="col-span-1 items-center justify-center w-4 h-4 cota-material rounded-full"
+            >
+              <p class="text-cota-primary text-xs">i</p>
+            </button>
+          </div>
         </div>
       </div>
     </div>
