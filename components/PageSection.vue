49f9fcedf21b32fd4428d13d39a0747822bfeb7d
Martin Westphal <martin@westphal.pw>
2024-02-05 09:49:46 +0100

    Added PageSection Component

---
+++ components/PageSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="['mt-12', withPadding ? 'px-6 sm:px-12' : '']">
+    <slot class="max-w-4xl px-8" name="headlines" />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  withPadding: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
+
+<style></style>
