20b579b544b861a2fe1f4f9c129aa212cc40ef9a
Martin Westphal <martin@westphal.pw>
2024-02-05 14:07:57 +0100

    fixed white background behind legal pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,45 +1,48 @@
 <template>
-  <head>
-    <meta name="theme-color" content="#020811">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-  </head>
-  <Navbar/>
   <div class="bg-cota-background">
-    <slot/>
+    <head>
+      <meta name="theme-color" content="#020811" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+    </head>
+    <Navbar />
+
+    <slot />
+
+    <FooterSection @submission-result="handleSubscriptionResult" id="footer" />
+    <NotificationComponent
+      :show="showNotification"
+      :message="notificationMessage"
+      :isSuccess="isSuccess"
+      @close="showNotification = false"
+    />
   </div>
-  <FooterSection @submission-result="handleSubscriptionResult" id="footer"/>
-  <NotificationComponent 
-    :show="showNotification" 
-    :message="notificationMessage" 
-    :isSuccess="isSuccess"
-    @close="showNotification = false"
-  />
 </template>
 <script setup lang="ts">
-
 useSeoMeta({
-  title: 'code.talks conference',
-  description: 'Home page of the code.talks Hamburg developer conference.',
-  ogTitle: 'code.talks conference',
-  ogDescription: 'Home page of the code.talks Hamburg developer conference.',
-  ogImage: '/cota_logo.png',
-})
+  title: "code.talks conference",
+  description: "Home page of the code.talks Hamburg developer conference.",
+  ogTitle: "code.talks conference",
+  ogDescription: "Home page of the code.talks Hamburg developer conference.",
+  ogImage: "/cota_logo.png",
+});
 
 useHead({
   htmlAttrs: {
-    lang: 'en'
+    lang: "en",
   },
   link: [
     {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon-16x16.png'
-    }
-  ]
-})
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-16x16.png",
+    },
+  ],
+});
 
-
-import { ref } from 'vue';
+import { ref } from "vue";
 
 type SubscriptionResult = {
   success: boolean;
@@ -47,7 +50,7 @@ type SubscriptionResult = {
 };
 
 const showNotification = ref(false);
-const notificationMessage = ref('');
+const notificationMessage = ref("");
 const isSuccess = ref(false);
 
 const handleSubscriptionResult = (result: SubscriptionResult) => {
@@ -55,8 +58,6 @@ const handleSubscriptionResult = (result: SubscriptionResult) => {
   isSuccess.value = result.success;
   showNotification.value = true;
 };
-
 </script>
 
-<style>
-</style>
+<style></style>
