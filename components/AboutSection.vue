dd9067c1b67cc8aa5ba315e034138002c1c99277
Martin Westphal <martin@westphal.pw>
2024-02-07 12:32:53 +0100

    Fixed margins in side images

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -30,7 +30,7 @@
           class="mt-14 flex justify-end gap-8 sm:-mt-36 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0"
         >
           <div
-            class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80"
+            class="ml-auto w-44 flex-none mr-12 space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80"
           >
             <div class="relative">
               <img
