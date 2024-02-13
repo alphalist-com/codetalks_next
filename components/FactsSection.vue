106101b987b26be74fa13fe7dbe3fed8a2a5c7a3
Martin Westphal <martin@westphal.pw>
2024-02-13 13:33:01 +0100

    Changed besucher

--- components/FactsSection.vue
+++ components/FactsSection.vue
@@ -21,8 +21,8 @@
 
 <script setup lang="ts">
 const stats = [
-  { id: 1, name: "Besucher", value: "3000" },
-  { id: 2, name: "Speaker", value: "130" },
+  { id: 1, name: "Visitors", value: "3000" },
+  { id: 2, name: "Speakers", value: "130" },
   { id: 3, name: "Talks", value: "120" },
   { id: 4, name: "Popcorn", value: "2T" },
 ];
