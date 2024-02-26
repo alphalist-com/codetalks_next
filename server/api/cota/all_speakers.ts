81b126f5fc8aaaeec1a94eb18470c61cd0e8acee
Martin Westphal <martin@westphal.pw>
2024-02-27 00:58:16 +0100

    fixed speakers not being fetched

--- server/api/cota/all_speakers.ts
+++ server/api/cota/all_speakers.ts
@@ -2,9 +2,14 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
 
   const body = await readBody(event);
-  const url = `https://app.codetalks.com/speaker/all-speakers?count=10&exclude_ids=${body.excludeIds}`;
+  let excludeIds = body.excludeIds || [];
+  const excludeIdsParam =
+    excludeIds.length > 0 ? `[${excludeIds.join(",")}]` : "[]";
 
-  console.log("exclude speakerIds: ", body.excludeIds);
+  console.log("exclude speakerIds: ", excludeIds);
+  const url = `https://app.codetalks.com/speaker/all-speakers?count=10&exclude_ids=${excludeIdsParam}`;
+
+  console.log(url);
 
   const response = await fetch(url, {
     headers: {
