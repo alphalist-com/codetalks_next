99dc05cefed176e449292084598a192865bce8db
Martin Westphal <martin@westphal.pw>
2024-03-13 14:19:53 +0100

    Better padding for codetalks website

--- components/TicketShopDialog.vue
+++ components/TicketShopDialog.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="fixed inset-0 z-40 flex items-center justify-center px-6 py-10">
+    <div
+      class="fixed inset-0 z-40 flex items-center justify-center px-6 pb-12 pt-6 md:px-20 md:py-12"
+    >
       <div class="fixed inset-0 bg-black/50" @click="$emit('close')"></div>
       <div
         class="text-cota-green relative z-50 max-h-full max-w-full overflow-x-auto overflow-y-auto rounded-xl border-2 border-cota-primary bg-cota-background text-sm font-semibold shadow-lg backdrop-blur-md backdrop-filter sm:mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-4xl"
